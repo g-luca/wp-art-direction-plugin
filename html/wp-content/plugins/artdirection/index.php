2a3d17180a251203aafa0570c8f9835d3fdfa2e3
Luca <luca.g@protonmail.ch>
2021-07-12 10:54:43 +0200

    added plugin index

---
+++ html/wp-content/plugins/artdirection/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ArtDirection
+ *
+ * @package           ArtDirection
+ * @copyright         2021
+ * @license           MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ArtDirection
+ * Description:       Plugin for art direction rules and resolution switching.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Project designed for the Course of Progettazione e Produzione Multimediale del Prof. Alberto Del Bimbo - Università degli studi di firenze; Idea by Andrea Ferracani; Development by Luca Graziotti & Alessio Magnanensi
+ * Text Domain:       wp-artdirection
+ * License:           MIT
+ */
